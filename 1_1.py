c5672eb5e6330c491229013ae72d91312448c7d5
dlydb <liuziyu1121@gmail.com>
2019-05-17 13:09:55 -0400

    1st commit

---
+++ 1_1.py
@@ -0,0 +1,11 @@
+def is_unique(str):
+    a_set = set()
+    for chr in str:
+        if chr in a_set:
+            return False
+        else:
+            a_set.add(chr)
+            
+    return True
+
+print (is_unique())
